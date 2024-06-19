001c171f8cb626c2e6e5749bd11e79cbffa4c57e
Thibault Valat <thib.valat@gmail.com>
2024-06-19 09:56:53 +0200

    init

---
+++ back/README.md
@@ -0,0 +1,1 @@
+# Projet Thibault Antoine Maxime
---
+++ back/src/main/java/com/projet/valatMoslehGrodet/controller/CacheController.java
@@ -0,0 +1,30 @@
+package com.projet.valatMoslehGrodet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cache")
+public class CacheController {
+    @Autowired
+    private CacheManager cacheManager;
+
+    @GetMapping
+    public Map<String, Object> getAllCaches() {
+        Map<String, Object> caches = new HashMap<>();
+        for (String cacheName : cacheManager.getCacheNames()) {
+            Cache cache = cacheManager.getCache(cacheName);
+            if (cache != null) {
+                caches.put(cacheName, cache.getNativeCache().toString());
+            }
+        }
+        return caches;
+    }
+}
