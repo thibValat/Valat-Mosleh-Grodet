7449780b309001dcc6e0089e2f9dabd49b666e07
AntoineMosleh <toni.mosleh@outlook.fr>
2024-06-21 11:10:41 +0200

    optimisation

--- back/src/main/java/com/projet/valatMoslehGrodet/repository/AccountRepository.java
+++ back/src/main/java/com/projet/valatMoslehGrodet/repository/AccountRepository.java
@@ -3,8 +3,14 @@ package com.projet.valatMoslehGrodet.repository;
 import com.projet.valatMoslehGrodet.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
     public Account findByEmail(String email);
+    public Account findByFirstName(String firstName);
+    public Account findByLastName(String lastName);
+    List<Account> findByFirstNameAndLastName(String firstName, String lastName);
+
 
 
 }
